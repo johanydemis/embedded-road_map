6047365c8ae54afd2b97d6b4d580251c3aeda85b
Johany Demis <johanydemis@gmail.com>
2026-03-22 00:46:23 -0600

    reallocation files

--- week1/arrays.c
+++ week1/arrays.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 void printArray(int *arr, int size) {
     for(int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+        printf("%d \n", arr[i]);
     }
 }
+
+void func(int arr[]) {
+    printf("Inside func sizeof(arr): %zu\n", sizeof(arr));
+}
+
 int main() {
     int arr[3] = {1, 2, 3};
     int *p = arr;
@@ -16,6 +21,8 @@ int main() {
     printf("*(p+2): %d\n", *(p+2));
 
     printArray(arr, 3);
-
+    printf("sizeof(arr): %lu\n", sizeof(arr));
+    printf("sizeof(p): %lu\n", sizeof(p));
+    func(arr);
     return 0;
 }
